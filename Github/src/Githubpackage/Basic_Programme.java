d0195d68cb6f21eade2431ba44c3e3a7e1418704
Basha23 <akhilbasha9494@gmail.com>
2022-10-17 15:21:27 +0530

    Basha done this commit.

---
+++ Github/src/Githubpackage/Basic_Programme.java
@@ -0,0 +1,16 @@
+package Githubpackage;
+
+public class Basic_Programme {
+
+	
+	int a=20;
+	int b=30;
+	 int c=a+b;
+	public static void main(String[] args) {
+	
+		System.out.println("i am from basic class");
+		
+		
+	}
+
+}
